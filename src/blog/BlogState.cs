a5ac3001f1ffa034f1c75f522574ec240ab56ce4
JerryBian <JerryBian@outlook.com>
2019-09-29 22:04:58 +0800

    add for #12

--- src/blog/BlogState.cs
+++ src/blog/BlogState.cs
@@ -1,9 +1,23 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
+using Humanizer;
 
 namespace Laobian.Blog
 {
     public class BlogState
     {
+        private static DateTime _startAt = DateTime.UtcNow;
+
+        public static void Init()
+        {
+            if (_startAt != default)
+            {
+                _startAt = DateTime.UtcNow;
+            }
+        }
+
         public static string NetCoreVersion => RuntimeInformation.FrameworkDescription;
+
+        public static string RunningInterval => (DateTime.UtcNow - _startAt).Humanize();
     }
 }
