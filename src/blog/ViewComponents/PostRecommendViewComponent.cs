d7e26789cfdca0cd218cb126a6b3bbca950f1771
JerryBian <JerryBian@outlook.com>
2019-10-24 22:18:29 +0800

    initial work

---
+++ src/blog/ViewComponents/PostRecommendViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Laobian.Share.BlogEngine;
+using Laobian.Share.Config;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Laobian.Blog.ViewComponents
+{
+    public class PostRecommendViewComponent : ViewComponent
+    {
+        private readonly AppConfig _appConfig;
+        private readonly IBlogService _blogService;
+
+        public PostRecommendViewComponent(IBlogService blogService, IOptions<AppConfig> appConfig)
+        {
+            _appConfig = appConfig.Value;
+            _blogService = blogService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string[] excludedLink)
+        {
+            var posts = _blogService.GetPosts()
+                .Where(p => p.IsPublic && (excludedLink == null || !excludedLink.Contains(p.Link)))
+                .ToList();
+            return View(posts);
+        }
+    }
+}
