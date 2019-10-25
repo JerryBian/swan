cca766753d5b76139793ba748b74f740e0f592ed
JerryBian <JerryBian@outlook.com>
2019-10-25 22:35:26 +0800

    fix #42

--- src/blog/ViewComponents/PostRecommendViewComponent.cs
+++ src/blog/ViewComponents/PostRecommendViewComponent.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Laobian.Blog.Models;
 using Laobian.Share.BlogEngine;
+using Laobian.Share.BlogEngine.Model;
 using Laobian.Share.Config;
+using Laobian.Share.Helper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
@@ -20,12 +23,69 @@ namespace Laobian.Blog.ViewComponents
             _blogService = blogService;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync(string[] excludedLink)
+        public async Task<IViewComponentResult> InvokeAsync(bool adminView, string excludedLink)
         {
-            var posts = _blogService.GetPosts()
-                .Where(p => p.IsPublic && (excludedLink == null || !excludedLink.Contains(p.Link)))
-                .ToList();
-            return View(posts);
+            List<BlogPost> posts;
+            if (adminView)
+            {
+                posts = _blogService.GetPosts()
+                    .Where(p => p.IsPublic)
+                    .ToList();
+            }
+            else
+            {
+                posts = _blogService.GetPosts().ToList();
+            }
+
+            var post = posts.FirstOrDefault(p => StringEqualsHelper.IgnoreCase(excludedLink, p.Link));
+            var postsWithWeight = new List<PostWithWeight>();
+            var latestPost = posts.OrderByDescending(p => p.CreationTimeUtc).FirstOrDefault();
+            foreach (var blogPost in posts)
+            {
+                if (blogPost == post)
+                {
+                    continue;
+                }
+
+                var postWithWeight = new PostWithWeight { Post = blogPost };
+                if (post == null)
+                {
+                    if (latestPost != null)
+                    {
+                        postWithWeight.TicksDiff = (blogPost.CreationTimeUtc - latestPost.CreationTimeUtc).Ticks;
+                    }
+                }
+                else
+                {
+                    if (blogPost.CategoryNames.Any(c => post.CategoryNames.Contains(c)))
+                    {
+                        postWithWeight.Weight += 0.7;
+                    }
+
+                    if (blogPost.TagNames.Any(t => post.TagNames.Contains(t)))
+                    {
+                        postWithWeight.Weight += 0.9;
+                    }
+                }
+
+                postsWithWeight.Add(postWithWeight);
+            }
+
+            double totalVisits = 1.0;
+            double totalTicksDiff = 1.0;
+            foreach (var postWithWeight in postsWithWeight)
+            {
+                totalVisits += postWithWeight.Post.Visits;
+                totalTicksDiff += postWithWeight.TicksDiff;
+            }
+
+            foreach (var postWithWeight in postsWithWeight)
+            {
+                postWithWeight.Weight -= postWithWeight.Post.Visits / (double)totalVisits;
+                postWithWeight.Weight += postWithWeight.TicksDiff / totalTicksDiff;
+            }
+
+            return View(postsWithWeight.OrderByDescending(pw => pw.Weight).Take(8).Select(pw=>pw.Post));
         }
     }
 }
