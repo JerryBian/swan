93763f70d87909e3d4ed94e9d43ae6cad1aed5b4
Jerry Bian <JerryBian@outlook.com>
2019-12-08 21:58:06 +0800

    close issue #90 (#91)

---
+++ src/blog/Models/PostViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Laobian.Share.Blog.Model;
+
+namespace Laobian.Blog.Models
+{
+    public class PostViewModel
+    {
+        public BlogPost Post { get; set; }
+
+        public BlogPost PrevPost { get; set; }
+
+        public BlogPost NextPost { get; set; }
+    }
+}
