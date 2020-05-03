a16dae8da6c49ab62719ce38d52c15f1207b64f6
Jerry Bian <JerryBian@outlook.com>
2020-05-03 19:56:02 +0800

    close #140: tweak git attributes

---
+++ src/share/Log/InMemoryLoggerExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Laobian.Share.Log
+{
+    public static class InMemoryLoggerExtension
+    {
+        public static ILoggingBuilder AddInMemoryLogger(this ILoggingBuilder builder)
+        {
+            builder.AddProvider(new InMemoryLoggerProvider());
+            return builder;
+        }
+    }
+}
