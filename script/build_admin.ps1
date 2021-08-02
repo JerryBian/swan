4c9ac3e535fcb93b7b8c8f48dbbc1bca8a4d7e9c
JerryBian <JerryBian@outlook.com>
2021-08-02 23:27:14 +0800

    init admin styles

--- script/build_admin.ps1
+++ script/build_admin.ps1
@@ -1,7 +1,10 @@
 $WWWRootLoc = Join-Path $PSScriptRoot .. src admin wwwroot
+$NodeModulesLoc = Join-Path $PSScriptRoot .. node_modules
 
-# Index page
-npx sass $(Join-Path $WWWRootLoc scss app.scss) $(Join-Path $WWWRootLoc index.css)
+
+npx sass $(Join-Path $WWWRootLoc scss index.scss) $(Join-Path $WWWRootLoc index.css)
+uglifycss --expand-vars --ugly-comments --output $(Join-Path $WWWRootLoc index.min.css) $(Join-Path $WWWRootLoc index.css)
+uglifyjs --compress -o $(Join-Path $WWWRootLoc script.min.js) $(Join-Path $NodeModulesLoc bootstrap dist js bootstrap.js)
 
 # Error page
 npx sass $(Join-Path $WWWRootLoc scss error.scss) $(Join-Path $WWWRootLoc error.css)
