2381687e76e8bd82418fadc4dd977fcffa071d86
JerryBian <JerryBian@outlook.com>
2021-08-06 10:49:44 +0800

    fix host issues for wait-for-it

--- script/start-after-api.sh
+++ script/start-after-api.sh
@@ -4,9 +4,7 @@
 set -e
 
 # Wait for the backend to be up, if we know where it is.
-if [ -n "$ENV_API_LOCAL_ENDPOINT" ]; then
-  /app/wait-for-it.sh "$ENV_API_LOCAL_ENDPOINT:${ENV_API_PORT:-5010}"
-fi
+/app/wait-for-it.sh api:5010 -t 300 --strict -- echo "api is up"
 
 # Run the main container command.
 exec "$@"
