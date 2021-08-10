df875e35e828eedf5a5d66ca66c035353ff90560
JerryBian <JerryBian@outlook.com>
2021-08-10 13:15:55 +0800

    fix API port

--- script/start-after-api.sh
+++ script/start-after-api.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Wait for the backend to be up, if we know where it is.
-/app/wait-for-it.sh api:5010 -t 300 --strict -- echo "api is up"
+/app/wait-for-it.sh api:5011 -t 300 --strict -- echo "api is up"
 
 # Run the main container command.
 exec "$@"
