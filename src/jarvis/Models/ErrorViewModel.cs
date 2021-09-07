59ddd6d14a364d947fb47dabde9cd0e4c892a367
JerryBian <JerryBian@outlook.com>
2021-09-07 22:42:55 +0800

    init 5.10

---
+++ src/jarvis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Laobian.Jarvis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
