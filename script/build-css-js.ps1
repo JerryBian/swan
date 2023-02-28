02da3a04f5b06e44116de8c406ccac16a404f6a4
JerryBian <JerryBian@outlook.com>
2023-02-28 22:49:28 +0800

    update

--- script/build-css-js.ps1
+++ script/build-css-js.ps1
@@ -12,7 +12,7 @@ Write-Output "[all]: Fonts copied"
 npx sass $(Join-Path $WWWRootLoc css style.scss) $(Join-Path $WWWRootLoc style.css)
 uglifycss --ugly-comments `
     --output $(Join-Path $WWWRootLoc style.min.css) `
-$(Join-Path $NodeModulesLoc \@highlightjs cdn-assets styles atom-one-light.min.css) `
+$(Join-Path $NodeModulesLoc \@highlightjs cdn-assets styles atom-one-dark.min.css) `
 $(Join-Path $WWWRootLoc style.css)
 Write-Output "[normal]: CSS completed"
 
