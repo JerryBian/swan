cfbc556961a3d3eb9575928d61a710a8ed646d32
JerryBian <JerryBian@outlook.com>
2023-10-20 10:42:39 +0800

    init tag/series

---
+++ src/web/Controllers/SeriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swan.Core.Helper;
+using Swan.Core.Service;
+
+namespace Swan.Web.Controllers
+{
+    public class SeriesController : Controller
+    {
+        private readonly ISwanService _swanService;
+
+        public SeriesController(ISwanService swanService)
+        {
+            _swanService = swanService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/series/{link}")]
+        public async Task<IActionResult> Get([FromRoute] string link)
+        {
+            var allSeries = await _swanService.GetBlogSeriesAsync();
+            var series = allSeries.FirstOrDefault(x => StringHelper.EqualsIgoreCase(x.Link, link));
+            if(series == null)
+            {
+                return NotFound();
+            }
+
+            return View("Detail", series);
+        }
+    }
+}
---
+++ src/web/Controllers/TagController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swan.Core.Helper;
+using Swan.Core.Service;
+
+namespace Swan.Web.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ISwanService _swanService;
+
+        public TagController(ISwanService swanService)
+        {
+            _swanService = swanService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/tag/{link}")]
+        public async Task<IActionResult> Get([FromRoute] string link)
+        {
+            var allTags = await _swanService.GetBlogTagsAsync();
+            var tag = allTags.FirstOrDefault(x => StringHelper.EqualsIgoreCase(x.Link, link));
+            if(tag == null)
+            {
+                return NotFound();
+            }
+
+            return View("Detail", tag);
+        }
+    }
+}
